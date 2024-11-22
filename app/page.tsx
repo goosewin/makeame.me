be5ade9c1d418ba9b62ed68dd550c23d8805a5b0
goosewin <daniel@webline.app>
2024-11-21 18:44:05 -0800

    change title

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import memes from "./data/memes";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-xl font-bold">pick your meme</h1>
+      <h1 className="text-xl font-bold">makeame.me</h1>
       <main className="max-w-[1200] grid grid-cols-3 col-auto gap-8 row-start-2 items-center sm:items-start">
         {memes?.map((meme) => (
           <Card className="h-[400] flex flex-col justify-center items-center" key={meme.id}>
